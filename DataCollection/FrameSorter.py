9aaac34fbc542a1dfd678c245fe44b3febfa4ffb
Michael Lance <michaelbraydenlance@gmail.com>
2024-06-21 13:50:18 -0600

    first commit

---
+++ DataCollection/FrameSorter.py
@@ -0,0 +1,7 @@
+# Video frame extractor & preprocessor using PIL and cv2
+# A Michael Lance & Amol Gupta production
+# 6/21/2024
+#-----------------------------------------------------------------------------------------------------------#
+import os
+import cv2
+from PIL import image
