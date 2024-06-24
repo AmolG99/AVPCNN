3f47de32e070315354b267e12f8b78b54bcc177c
Michael Lance <michaelbraydenlance@gmail.com>
2024-06-24 15:12:16 -0600

    extracting video frames as png files

--- DataCollection/FrameSorter.py
+++ DataCollection/FrameSorter.py
@@ -1,7 +1,35 @@
 # Video frame extractor & preprocessor using PIL and cv2
 # A Michael Lance & Amol Gupta production
 # 6/21/2024
+# 6/24/2024
 #-----------------------------------------------------------------------------------------------------------#
 import os
 import cv2
-from PIL import image
+from PIL import Image
+
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    raise Exception("camera'nt")
+
+output_dir = 'captured_frames'
+os.makedirs(output_dir, exist_ok=True)
+
+frame_count = 0
+
+while True:
+    ret, frame = cap.read()
+    
+    if not ret:
+        raise Exception("Couldn't read no frame")
+    
+    frame_filename = os.path.join(output_dir, f'frame_{frame_count:04d}.png')
+    cv2.imwrite(frame_filename, frame)
+    frame_count += 1
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+# Release the webcam and close windows
+cap.release()
+cv2.destroyAllWindows()
---
+++ DataCollection/hand.py
@@ -0,0 +1,8 @@
+class HandData:    
+    def __init__(self), top, bottom, left, right, centerX:
+        self.top = top
+        self.bottom = bottom
+        self.left = left
+        self.right = rightself.centerx = centerx
+        isInframe = False
+    
