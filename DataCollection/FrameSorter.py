b8e621d5cf7554427cfe265f403b98254fb14a93
Michael Lance <michaelbraydenlance@gmail.com>
2024-06-25 13:05:20 -0600

    faster master

--- DataCollection/FrameSorter.py
+++ DataCollection/FrameSorter.py
@@ -19,6 +19,7 @@ if not cap.isOpened():
     raise Exception("camera'nt")
 
 frame_count = 0
+frame_interval = 3
 video_length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
 for index in range(video_length):
--- DataCollection/extractframe.py
+++ DataCollection/extractframe.py
@@ -1,12 +1,13 @@
 import cv2
 import os
 
-def extractframe(cap, frame_count, output_dir):
+def extractframe(cap, frame_count, frame_interval, output_dir):
     ret, frame = cap.read()
     
     if not ret:
         raise Exception("Couldn't read no frame")
     
-    frame_filename = os.path.join(output_dir, f'frame_{frame_count:04d}.png')
-    cv2.imwrite(frame_filename, frame)
+    if frame_count % frame_interval == 0:
+        frame_filename = os.path.join(output_dir, f'frame_{frame_count:04d}.png')
+        cv2.imwrite(frame_filename, frame)
     
